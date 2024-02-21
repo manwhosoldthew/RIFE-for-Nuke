473244dcb881408ec08e56fcbe0501c349c2243f
Rafael Silva <rafael@rafael.ai>
2024-02-20 21:42:33 -0500

    undo hack to make it work with nuke 13
    Nuke 13.2v8 fixes a bug where the Inference node would error out
    if the output had more than 4 channels. This commit undoes the hack,
    assuming the user will use the latest Nuke version.

--- nuke_rife.py
+++ nuke_rife.py
@@ -45,58 +45,27 @@ def trace_rife():
 
             # Return the optical flow and mask
             if self.optical_flow:
-                flow = flow[:, :, :h, :w]
-                stmap_x = (
-                    torch.linspace(0.0, 1.0, w, device=device).view(1, 1, 1, w).expand(1, -1, h, -1)
-                )
-                stmap_y = (
-                    torch.linspace(1.0, 0.0, h, device=device).view(1, 1, h, 1).expand(1, -1, -1, w)
-                )
-                stmap_x_y = torch.cat([stmap_x, stmap_y, stmap_x, stmap_y], 1).to(device)
-
-                displacement = torch.cat(
-                    [
-                        flow[:, 0:1, :, :] * 0.5 / ((w - 1.0) / 2.0),
-                        flow[:, 1:2, :, :] * -0.5 / ((h - 1.0) / 2.0),
-                        flow[:, 2:3, :, :] * 0.5 / ((w - 1.0) / 2.0),
-                        flow[:, 3:4, :, :] * -0.5 / ((h - 1.0) / 2.0),
-                    ],
-                    1,
-                )
-
-                stmap = stmap_x_y + displacement
-
-                stmap_packed = torch.zeros((b, 3, h, w), dtype=torch.float32)
-                # Since Nuke 13 throws an error if we return an image with 5 channels,
-                # we are packing both the flow channels into a single one and
-                # returning a 3 channel image - stmap_fw, stmap_bw, and mask.
-
-                # Scale numbers to use 11 and 12 bits respectively, shift and combine
-                stmap_fw_x = (stmap[:, 0] * (2**11 - 1)).to(torch.int32)
-                stmap_fw_y = (stmap[:, 1] * (2**12 - 1)).to(torch.int32)
-                combined_fw = (stmap_fw_x << 12) | stmap_fw_y
-                stmap_packed[:, 0] = torch.as_tensor(combined_fw, dtype=torch.float32)
-
-                stmap_bw_x = (stmap[:, 2] * (2**11 - 1)).to(torch.int32)
-                stmap_bw_y = (stmap[:, 3] * (2**12 - 1)).to(torch.int32)
-                combined_bw = (stmap_bw_x << 12) | stmap_bw_y
-                stmap_packed[:, 1] = torch.as_tensor(combined_bw, dtype=torch.float32)
-
-                stmap_packed[:, 2] = mask[:, 0, :h, :w]
-                return stmap_packed.contiguous()
-
+                return torch.cat((flow[:, :, :h, :w], mask[:, :, :h, :w]), 1).contiguous()
+            
             # Return the interpolated frames
-            return image[:, :3, :h, :w].contiguous()
-
-    model_file = "./nuke/Cattery/RIFE/RIFE_n13.pt"
-    # model_file = "./nuke/Cattery/RIFE/RIFE_n13_ref.pt"
+            alpha = torch.ones((b, 1, h, w), dtype=x.dtype, device=device)
+            return torch.cat((image[:, :, :h, :w], alpha), dim=1).contiguous()
+    
     with torch.jit.optimized_execution(True):
         rife_nuke = torch.jit.script(FlowNetNuke())
+        # model_file = "./nuke/Cattery/RIFE/RIFE_n13.pt"
+        model_file = "./nuke/Cattery/RIFE/RIFE_n13_speed.pt"
+        
+        # Take advantage of the new onednn fusion in PyTorch 2.0
+        if not torch.__version__.startswith("1.6"):
+            model_file = "./nuke/Cattery/RIFE/RIFE_n14.pt"
+            torch.jit.enable_onednn_fusion(True)
+            rife_nuke = torch.jit.freeze(rife_nuke.eval(), preserved_attrs=["optical_flow", "timestep", "scale"])
+
         rife_nuke.save(model_file)
         LOGGER.info(rife_nuke.code)
         LOGGER.info(rife_nuke.graph)
         LOGGER.info("Traced flow saved: %s", model_file)
-
 
 if __name__ == "__main__":
     trace_rife()
